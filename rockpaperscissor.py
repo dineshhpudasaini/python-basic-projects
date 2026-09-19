dec2de29bfba7efd12acd19eaed0dd5cc16e493a
Dinesh <dineshhpudasaini@gmail.com>
2026-09-19 22:13:07 +0545

    completedd

--- rockpaperscissor.py
+++ rockpaperscissor.py
@@ -2,13 +2,18 @@ import random
 
 choices = ["rock","paper","scissor"]
 
-user_choice = input("enter your choice among rock paper and scissor: ")
+user_choice = input("enter your choice among rock paper and scissor: ").lower()
 comp_choice = random.choice(choices)
 
-print(f"user choice = {user_choice} and compuer choice = {comp_choice}")
+print(f"Your choice = {user_choice} and computer choice = {comp_choice}")
 
 if(comp_choice == user_choice):
     print("Tie")
 
-elif(comp_choice == rock && user_choice == scissor )||(comp_choice == paper && user_choice ==rock):
+elif(comp_choice == "rock" and user_choice == "scissor" ) or (comp_choice == "paper" and user_choice == "rock"):
     print("Computer won")
+    
+elif(user_choice == "rock" and comp_choice == "scissor" ) or (user_choice == "paper" and comp_choice == "rock"):
+    print("You won")
+else:
+    print("invalid request try again")
